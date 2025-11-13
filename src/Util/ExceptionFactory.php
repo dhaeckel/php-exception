3de416dec0b2e4c0cc33737ae4c2a4d869cf4fbf
Dominik Häckel <65503128+dhaeckel@users.noreply.github.com>
2025-11-13 22:23:54 +0100

    Allow passing msg to newErrExceptionFromlastErr

--- src/Util/ExceptionFactory.php
+++ src/Util/ExceptionFactory.php
@@ -11,19 +11,22 @@ final class ExceptionFactory
      * Create an \ErrorException from the last error that occurred. Will pick up the last error if
      * null is passed
      * @param array{type: int, message: string, file: string, line: int}|null $lastErr
+     * @param string $msg Optional message prefix, if last err is null, only this is used
      *
      * @link https://php.net/manual/en/function.error-get-last.php
      */
-    public static function newErrExceptionFromLastErr(?array $lastErr = null): \ErrorException
-    {
+    public static function newErrExceptionFromLastErr(
+        ?array $lastErr = null,
+        string $msg = '',
+    ): \ErrorException {
         $lastErr ??= \error_get_last();
         if ($lastErr === null) {
             // throw blank err exception when last err is not set
-            return new \ErrorException();
+            return new \ErrorException($msg);
         }
 
         return new \ErrorException(
-            $lastErr['message'],
+            $msg !== '' ? $msg . ': ' . $lastErr['message'] : $lastErr['message'],
             severity: $lastErr['type'],
             filename: $lastErr['file'],
             line: $lastErr['line'],
--- test/Util/ErrorExceptionTest.php
+++ test/Util/ErrorExceptionTest.php
@@ -29,6 +29,28 @@ final class ErrorExceptionTest extends TestCase
         );
     }
 
+    public function testNewErrExceptionFromLastErrWithMsg(): void
+    {
+        \trigger_error('test msg');
+        $lastErr = \error_get_last();
+        $e = ExceptionFactory::newErrExceptionFromLastErr(msg: 'error occurred');
+        $this->assertEquals(
+            new \ErrorException(
+                'error occurred: test msg',
+                severity: $lastErr['type'],
+                filename: $lastErr['file'],
+                line: $lastErr['line'],
+            ),
+            $e,
+        );
+    }
+
+    public function testWithMsgOnly(): void
+    {
+        $e = ExceptionFactory::newErrExceptionFromLastErr(msg: 'error occurred');
+        $this->assertEquals(new \ErrorException('error occurred'), $e);
+    }
+
     public function testNewErrExceptionWithoutLastErr(): void
     {
         $e = ExceptionFactory::newErrExceptionFromLastErr();
